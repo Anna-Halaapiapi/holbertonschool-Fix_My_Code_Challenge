16dea80b3382fa5953fdb239e86c15f5a1a2c7f5
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-04-05 21:31:19 +1000

    t4 - fix pointer reassignment and free statement

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
@@ -8,7 +9,7 @@
  * @index: The index of the node to delete
  *
  * Return: 1 on success, -1 on failure
- */
+ */ 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *saved_head;
@@ -43,11 +44,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
+		if ((*head)->next != NULL) /* head is not last in list */
+			{
+			(*head)->next->prev = (*head)->prev; /* point head's next's prev to head's prev */
+			(*head)->prev->next = (*head)->next; /* point head's prev's next to head's next */
+			}
+		else /* head is last on list (head's next is NULL) */
+			{
+			(*head)->prev->next = NULL; /* point head's prev's next to NULL (make it the tail) */
+			}
 		*head = saved_head;
+		free(*head); /*  free node after pointer reassignments*/
 	}
 	return (1);
 }
